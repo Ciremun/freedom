31e2b3d322c0f8b15924b0f83e0614a80492a2e5
Ciremun <Ciremun@yandex.ru>
2022-12-30 03:05:18 +0300

    clr cleanup

--- freedom/prejit.cpp
+++ freedom/prejit.cpp
@@ -17,33 +17,37 @@ bool prejit_all()
     if (!CLRCreateInstance)
         return false;
 
-    CLRCreateInstance(CLSID_CLRMetaHost, IID_ICLRMetaHost, (void **)&pMetaHost);
-
-    if (pMetaHost->GetRuntime(L"v4.0.30319", IID_ICLRRuntimeInfo, (void **)&pRuntimeInfo) == S_OK)
+    if (CLRCreateInstance(CLSID_CLRMetaHost, IID_ICLRMetaHost, (void **)&pMetaHost) == S_OK)
     {
-        if (pRuntimeInfo->GetInterface(CLSID_CLRRuntimeHost, IID_ICLRRuntimeHost, (void **)&pClrRuntimeHost) == S_OK)
+        if (pMetaHost->GetRuntime(L"v4.0.30319", IID_ICLRRuntimeInfo, (void **)&pRuntimeInfo) == S_OK)
         {
-            pClrRuntimeHost->Start();
-            DWORD dwRet = 0;
+            if (pRuntimeInfo->GetInterface(CLSID_CLRRuntimeHost, IID_ICLRRuntimeHost, (void **)&pClrRuntimeHost) == S_OK)
+            {
+                pClrRuntimeHost->Start();
+                DWORD dwRet = 0;
 
-            wchar_t module_path[MAX_PATH * 2];
-            DWORD module_path_length = GetModuleFileNameW(g_module, module_path, MAX_PATH * 2);
-            if (module_path_length == 0)
-                return false;
+                wchar_t module_path[MAX_PATH * 2];
+                DWORD module_path_length = GetModuleFileNameW(g_module, module_path, MAX_PATH * 2);
+                if (module_path_length == 0)
+                    return false;
 
-            DWORD backslash_index = module_path_length - 1;
-            while (backslash_index)
-                if (module_path[--backslash_index] == '\\')
-                    break;
+                DWORD backslash_index = module_path_length - 1;
+                while (backslash_index)
+                    if (module_path[--backslash_index] == '\\')
+                        break;
 
-            memcpy(module_path + backslash_index + 1, L"prejit.dll", 10 * sizeof(WCHAR) + 1);
+                memcpy(module_path + backslash_index + 1, L"prejit.dll", 10 * sizeof(WCHAR) + 1);
 
-            HRESULT result = pClrRuntimeHost->ExecuteInDefaultAppDomain(module_path, L"Freedom.PreJit", L"prejit_all", L"", &dwRet);
-            if (result != S_OK)
-                FR_ERROR_FMT("pClrRuntimeHost->ExecuteInDefaultAppDomain failed, error code: 0x%X", result);
-            pClrRuntimeHost->Stop();
-            pClrRuntimeHost->Release();
+                HRESULT result = pClrRuntimeHost->ExecuteInDefaultAppDomain(module_path, L"Freedom.PreJit", L"prejit_all", L"", &dwRet);
+                if (result != S_OK)
+                    FR_ERROR_FMT("pClrRuntimeHost->ExecuteInDefaultAppDomain failed, error code: 0x%X", result);
+                pClrRuntimeHost->Stop();
+                pClrRuntimeHost->Release();
+            }
+            pRuntimeInfo->Release();
         }
+        pMetaHost->Release();
     }
+
     return true;
 }
