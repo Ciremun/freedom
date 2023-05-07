894392f9917cc3445f358132f2d7de5f7c7df1c1
dafijo <79472612+dafijo@users.noreply.github.com>
2023-05-07 22:08:15 +0200

    Update injector.cpp

--- injector.cpp
+++ injector.cpp
@@ -1,28 +1,19 @@
-// This is an independent project of an individual developer. Dear PVS-Studio, please check it.
-// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
+#include <iostream>
+#include <Windows.h>
+#include <TlHelp32.h>
+#include <vector>
 
-#include <windows.h>
-#include <tlhelp32.h>
-
-#include <stdio.h>
-#include <string.h>
-
-DWORD get_process_id(const wchar_t *process_name)
-{
+DWORD get_process_id(const wchar_t* process_name) {
     DWORD process_id = 0;
     HANDLE hSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
 
-    if (hSnap != INVALID_HANDLE_VALUE)
-    {
+    if (hSnap != INVALID_HANDLE_VALUE) {
         PROCESSENTRY32 procEntry;
         procEntry.dwSize = sizeof(procEntry);
 
-        if (Process32First(hSnap, &procEntry))
-        {
-            do
-            {
-                if (!_wcsicmp(procEntry.szExeFile, process_name))
-                {
+        if (Process32First(hSnap, &procEntry)) {
+            do {
+                if (!_wcsicmp(procEntry.szExeFile, process_name)) {
                     process_id = procEntry.th32ProcessID;
                     break;
                 }
@@ -33,50 +24,60 @@ DWORD get_process_id(const wchar_t *process_name)
     return process_id;
 }
 
-int wmain(int argc, wchar_t **argv, wchar_t **envp)
-{
-    wchar_t *process_name = argc > 1 ? argv[1] : L"osu!.exe";
-    DWORD process_id = get_process_id(process_name);
-    if (process_id == 0)
-    {
-        fprintf(stderr, "get_process_id failed: launch %S first!\n", process_name);
-        return 1;
-    }
-
-    wchar_t *dll_name = argc > 2 ? argv[2] : L"freedom.dll";
-    static wchar_t module_path[MAX_PATH * 2];
-    DWORD module_path_length = GetFullPathNameW(dll_name, MAX_PATH * 2, module_path, NULL);
-    if (module_path_length == 0)
-    {
-        fprintf(stderr, "GetFullPathNameA failed: %ld\n", GetLastError());
-        return 1;
-    }
-
-    HANDLE hProc = OpenProcess(PROCESS_ALL_ACCESS, 0, process_id);
-    if (hProc != NULL)
-    {
-        void *loc = VirtualAllocEx(hProc, 0, module_path_length * sizeof(wchar_t), MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
-        if (loc)
-        {
-            if (WriteProcessMemory(hProc, loc, module_path, module_path_length * sizeof(wchar_t), 0))
-            {
-                HANDLE hThread = CreateRemoteThread(hProc, 0, 0, (LPTHREAD_START_ROUTINE)LoadLibraryW, loc, 0, 0);
-                if (hThread)
-                    CloseHandle(hThread);
-                else
-                    fprintf(stderr, "CreateRemoteThread failed: %ld\n", GetLastError());
+std::vector<uintptr_t> find_all_addresses(HANDLE hProcess, const uintptr_t& base_address, const int& value) {
+    std::vector<uintptr_t> addresses;
+    uintptr_t offset = 0;
+    MEMORY_BASIC_INFORMATION mem_info = { 0 };
+    while (VirtualQueryEx(hProcess, (LPCVOID)(base_address + offset), &mem_info, sizeof(mem_info)) == sizeof(mem_info)) {
+        if ((mem_info.State & MEM_COMMIT) && (mem_info.Protect & (PAGE_READWRITE | PAGE_WRITECOPY)) && !(mem_info.Protect & PAGE_GUARD)) {
+            char* buffer = new char[mem_info.RegionSize];
+            SIZE_T bytesRead;
+            if (ReadProcessMemory(hProcess, mem_info.BaseAddress, buffer, mem_info.RegionSize, &bytesRead)) {
+                for (uintptr_t i = 0; i < bytesRead - sizeof(int); ++i) {
+                    if (*(int*)(buffer + i) == value) {
+                        addresses.push_back(base_address + offset + i);
+                    }
+                }
             }
-            else
-                fprintf(stderr, "WriteProcessMemory failed: %ld\n", GetLastError());
+            delete[] buffer;
         }
-        else
-            fprintf(stderr, "VirtualAllocEx failed: %ld\n", GetLastError());
+        offset += mem_info.RegionSize;
     }
-    else
-        fprintf(stderr, "OpenProcess failed: %ld\n", GetLastError());
-
-    if (hProc)
-        CloseHandle(hProc);
-
-    return 0;
+    return addresses;
 }
+
+int main() {
+    wchar_t* process_name = L"osu!.exe";
+    DWORD process_id = get_process_id(process_name);
+
+    if (process_id == 0) {
+        std::cerr << "Could not find process " << process_name << "!" << std::endl;
+        return 1;
+    }
+
+    HANDLE hProcess = OpenProcess(PROCESS_VM_READ | PROCESS_VM_WRITE | PROCESS_VM_OPERATION, FALSE, process_id);
+    if (hProcess == NULL) {
+        std::cerr << "Failed to open process with ID " << process_id << "!" << std::endl;
+        return 1;
+    }
+
+    MODULEINFO module_info = { 0 };
+    HMODULE hModule = GetModuleHandle(NULL);
+    if (hModule == NULL) {
+        std::cerr << "Failed to get handle to current module!" << std::endl;
+        CloseHandle(hProcess);
+        return 1;
+    }
+    if (!GetModuleInformation(hProcess, hModule, &module_info, sizeof(module_info))) {
+        std::cerr << "Failed to get module information for current module!" << std::endl;
+        CloseHandle(hProcess);
+        return 1;
+    }
+
+    uintptr_t base_address = (uintptr_t)module_info.lpBaseOfDll;
+    int value = 0;
+
+    std::vector<uintptr_t> addresses = find_all_addresses(hProcess, base_address, value);
+    if (addresses.empty()) {
+        std::cerr << "Could not find any matching addresses!" << std::endl;
+        CloseHandle
