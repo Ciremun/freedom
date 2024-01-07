d3a33826455f18312c0664ab01771958b8912b68
Ciremun <Ciremun@yandex.ru>
2024-01-07 09:17:50 +0300

    add rebuild flag

--- nobuild.c
+++ nobuild.c
@@ -121,7 +121,7 @@ static inline void bake_utils_dll()
 
 static void build_freedom_dll()
 {
-    if (!PATH_EXISTS("utils.dll") || is_path1_modified_after_path2("freedom/utils.cs", "utils.dll"))
+    if (!PATH_EXISTS("utils.dll") || rebuild_flag || is_path1_modified_after_path2("freedom/utils.cs", "utils.dll"))
     {
         CMD("csc", "/unsafe", "/nologo", "/optimize", "/target:library", "/out:utils.dll", "freedom/utils.cs");
         bake_utils_dll();
@@ -139,7 +139,7 @@ static void build_freedom_dll()
             }
         },
         DLL_DIRS);
-    if (!PATH_EXISTS("freedom_injector.exe") || is_path1_modified_after_path2("injector.cpp", "freedom_injector.exe"))
+    if (!PATH_EXISTS("freedom_injector.exe") || rebuild_flag || is_path1_modified_after_path2("injector.cpp", "freedom_injector.exe"))
     {
         CMD("cl", "/DWIN32_LEAN_AND_MEAN", "/DNDEBUG", "/DUNICODE", "/std:c++latest", "/MT", "/O2", "/EHsc", "/nologo", "/Fe:freedom_injector.exe", "injector.cpp", "/link", "/MACHINE:x86");
     }
