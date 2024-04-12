7cab476311412c820d737802476c3bdf21ffe7f9
Ciremun <Ciremun@yandex.ru>
2024-04-12 17:50:23 +0300

    start build refactor

--- nobuild.c
+++ nobuild.c
@@ -49,6 +49,12 @@
         }                                                         \
     } while (0)
 
+static inline void bake_utils_dll();
+static void async_obj_foreach_file_in_dirs(Cstr first, ...);
+static inline void set_git_commit_hash();
+static inline bool find_osu_exe(LPWSTR osu_exe_path);
+static inline bool launch_osu(LPWSTR osu_exe_path);
+
 int *standalone_flag = 0;
 int *debug_flag = 0;
 int *rebuild_flag = 0;
@@ -58,124 +64,28 @@ int *console_flag = 0;
 
 int do_link = 0;
 char define_commit_hash[32] = {0};
-
 wchar_t osu_exe_path[MAX_PATH * 2] = {0};
 
-static inline bool find_osu_exe(LPWSTR osu_exe_path)
+static void build_freedom_dll()
 {
-    PWSTR roaming_path;
-    if (SHGetKnownFolderPath(&FOLDERID_RoamingAppData, 0, NULL, &roaming_path) != S_OK)
-        return false;
-    const wchar_t *append_osu_lnk = L"\\Microsoft\\Windows\\Start Menu\\Programs\\osu!.lnk";
-    size_t new_length = wcslen(roaming_path) + wcslen(append_osu_lnk) + sizeof(WCHAR);
-    LPVOID new_ptr = CoTaskMemRealloc(roaming_path, new_length * sizeof(WCHAR));
-    if (!new_ptr)
+    if (!PATH_EXISTS("utils.dll") || *rebuild_flag || is_path1_modified_after_path2("freedom/utils.cs", "utils.dll"))
     {
-        CoTaskMemFree(roaming_path);
-        goto return_false;
+        CMD("csc", "/nologo", "/optimize", "/target:library", "/out:utils.dll", "freedom/utils.cs");
+        bake_utils_dll();
     }
-    roaming_path = (PWSTR)new_ptr;
-    wcscat_s(roaming_path, new_length, append_osu_lnk);
-    IShellLinkW *psl;
-    CoInitialize(NULL);
-    HRESULT hr = CoCreateInstance(&CLSID_ShellLink, NULL, CLSCTX_INPROC_SERVER, &IID_IShellLinkW, (LPVOID *)&psl);
-    if (SUCCEEDED(hr))
-    {
-        IPersistFile *ppf;
-        hr = IShellLinkW_QueryInterface(psl, &IID_IPersistFile, &ppf);
-        if (SUCCEEDED(hr))
-        {
-            hr = IPersistFile_Load(ppf, roaming_path, STGM_READ);
-            if (SUCCEEDED(hr))
-            {
-                WIN32_FIND_DATAW wfd;
-                IShellLinkW_GetPath(psl, osu_exe_path, MAX_PATH * 2, &wfd, SLGP_UNCPRIORITY | SLGP_RAWPATH);
-            }
+    async_obj_foreach_file_in_dirs(DLL_DIRS, NULL);
+    OBJS_FOR_DIRS(
+        objs, {
+            if (*debug_flag)
+                { CALL_LINK(objs, "LINK", "/DLL", "/OUT:freedom.dll", MSVC_LINK_DEBUG_FLAGS); }
             else
-                goto return_false;
-        }
-        else
-            goto return_false;
-    }
-    else
-        goto return_false;
-
-    CoUninitialize();
-    CoTaskMemFree(roaming_path);
-    return true;
-
-return_false:
-    CoUninitialize();
-    CoTaskMemFree(roaming_path);
-    return false;
-}
-
-static inline bool launch_osu(LPWSTR osu_exe_path)
-{
-    STARTUPINFOW siStartInfo;
-    ZeroMemory(&siStartInfo, sizeof(siStartInfo));
-    siStartInfo.cb = sizeof(STARTUPINFOW);
-    siStartInfo.hStdError = GetStdHandle(STD_ERROR_HANDLE);
-    siStartInfo.hStdOutput = GetStdHandle(STD_OUTPUT_HANDLE);
-    siStartInfo.hStdInput = GetStdHandle(STD_INPUT_HANDLE);
-    siStartInfo.dwFlags |= STARTF_USESTDHANDLES;
-    PROCESS_INFORMATION piProcInfo;
-    ZeroMemory(&piProcInfo, sizeof(PROCESS_INFORMATION));
-    return CreateProcessW(NULL, osu_exe_path, NULL, NULL, TRUE, 0, NULL, NULL, &siStartInfo, &piProcInfo);
-}
-
-static inline void remove_object_files()
-{
-    FOREACH_FILE_IN_DIR(file, ".", {
-        if (ENDS_WITH(file, ".obj"))
-            RM(file);
-    });
-}
-
-static void async_obj_foreach_file_in_dir(Pid *proc, size_t *proc_count, Cstr directory)
-{
-    FOREACH_FILE_IN_DIR(file, directory,
+                { CALL_LINK(objs, "LINK", "/DLL", "/OUT:freedom.dll", MSVC_LINK_RELEASE_FLAGS); }
+        },
+        DLL_DIRS);
+    if (!PATH_EXISTS("freedom_injector.exe") || *rebuild_flag || is_path1_modified_after_path2("injector.cpp", "freedom_injector.exe"))
     {
-        if (ENDS_WITH(file, ".cpp"))
-        {
-            Cstr src = CONCAT(directory, strdup(file));
-            Cstr obj = CONCAT(NOEXT(file), ".obj");
-            if (!PATH_EXISTS(obj) ||
-                (*rebuild_flag || is_path1_modified_after_path2(src, obj)))
-            {
-                do_link = 1;
-                Cstr_Array line = *debug_flag ? cstr_array_make("cl", MSVC_DEBUG_FLAGS, src, "/c", NULL) :
-                                               cstr_array_make("cl", MSVC_RELEASE_FLAGS, src, "/c", NULL);
-                if (define_commit_hash[0] != '\0')
-                    line = cstr_array_append(line, define_commit_hash);
-                if (*debug_flag)
-                    line = cstr_array_append(line, "/DFR_DEBUG");
-                if (*console_flag)
-                    line = cstr_array_append(line, "/DFR_LOG_TO_CONSOLE");
-                Cmd cmd = {.line = line};
-                INFO("CMD: %s", cmd_show(cmd));
-                proc[(*proc_count)++] = cmd_run_async(cmd, NULL, NULL);
-            }
-        }
-    });
-}
-
-static void async_obj_foreach_file_in_dirs(Cstr first, ...)
-{
-    Pid proc[PROCESSES_CAPACITY];
-    size_t proc_count = 0;
-    async_obj_foreach_file_in_dir(proc, &proc_count, first);
-    va_list args;
-    va_start(args, first);
-    for (Cstr directory = va_arg(args, Cstr);
-         directory != NULL;
-         directory = va_arg(args, Cstr))
-    {
-        async_obj_foreach_file_in_dir(proc, &proc_count, directory);
+        CMD("cl", "/DWIN32_LEAN_AND_MEAN", "/DNDEBUG", "/DUNICODE", "/std:c++latest", "/MT", "/O2", "/EHsc", "/nologo", "/Fe:freedom_injector.exe", "injector.cpp", "/link", "/MACHINE:x86");
     }
-    va_end(args);
-    for (size_t i = 0; i < proc_count; ++i)
-        pid_wait(proc[i]);
 }
 
 static void build_standalone()
@@ -201,79 +111,6 @@ static inline void bake_utils_dll()
     bool use_compression = false;
     bool use_static = true;
     binary_to_compressed_c("utils.dll", PATH("include", "baked_utils_dll.h"), "utils_dll", use_base85_encoding, use_compression, use_static);
-}
-
-static inline void set_git_commit_hash()
-{
-    HANDLE hChildStd_OUT_Rd = NULL;
-    HANDLE hChildStd_OUT_Wr = NULL;
-
-    SECURITY_ATTRIBUTES saAttr;
-    saAttr.nLength = sizeof(SECURITY_ATTRIBUTES);
-    saAttr.bInheritHandle = TRUE;
-    saAttr.lpSecurityDescriptor = NULL;
-
-    CreatePipe(&hChildStd_OUT_Rd, &hChildStd_OUT_Wr, &saAttr, 0);
-    SetHandleInformation(hChildStd_OUT_Rd, HANDLE_FLAG_INHERIT, 0);
-
-    TCHAR szCmdline[] = TEXT("git rev-parse --short HEAD");
-    PROCESS_INFORMATION piProcInfo;
-    STARTUPINFO siStartInfo;
-
-    ZeroMemory(&piProcInfo, sizeof(PROCESS_INFORMATION));
-    ZeroMemory(&siStartInfo, sizeof(STARTUPINFO));
-
-    siStartInfo.cb = sizeof(STARTUPINFO);
-    siStartInfo.hStdError = hChildStd_OUT_Wr;
-    siStartInfo.hStdOutput = hChildStd_OUT_Wr;
-    siStartInfo.hStdInput = NULL;
-    siStartInfo.dwFlags |= STARTF_USESTDHANDLES;
-
-    CreateProcess(NULL, szCmdline, NULL, NULL, TRUE, 0, NULL, NULL, &siStartInfo, &piProcInfo);
-
-    WaitForSingleObject(piProcInfo.hProcess, INFINITE);
-
-    DWORD exit_status;
-    GetExitCodeProcess(piProcInfo.hProcess, &exit_status);
-
-    CloseHandle(piProcInfo.hProcess);
-    CloseHandle(piProcInfo.hThread);
-    CloseHandle(hChildStd_OUT_Wr);
-
-    if (exit_status == 0)
-    {
-        DWORD dwRead;
-        char git_commit_hash[16] = {0};
-        ReadFile(hChildStd_OUT_Rd, git_commit_hash, 7, &dwRead, NULL);
-        if (dwRead > 0)
-            stbsp_snprintf(define_commit_hash, sizeof(define_commit_hash), "/DGIT_COMMIT_HASH=%s", git_commit_hash);
-    }
-}
-
-static void build_freedom_dll()
-{
-    if (!PATH_EXISTS("utils.dll") || *rebuild_flag || is_path1_modified_after_path2("freedom/utils.cs", "utils.dll"))
-    {
-        CMD("csc", "/nologo", "/optimize", "/target:library", "/out:utils.dll", "freedom/utils.cs");
-        bake_utils_dll();
-    }
-    async_obj_foreach_file_in_dirs(DLL_DIRS, NULL);
-    OBJS_FOR_DIRS(
-        objs, {
-            if (*debug_flag)
-            {
-                CALL_LINK(objs, "LINK", "/DLL", "/OUT:freedom.dll", MSVC_LINK_DEBUG_FLAGS);
-            }
-            else
-            {
-                CALL_LINK(objs, "LINK", "/DLL", "/OUT:freedom.dll", MSVC_LINK_RELEASE_FLAGS);
-            }
-        },
-        DLL_DIRS);
-    if (!PATH_EXISTS("freedom_injector.exe") || *rebuild_flag || is_path1_modified_after_path2("injector.cpp", "freedom_injector.exe"))
-    {
-        CMD("cl", "/DWIN32_LEAN_AND_MEAN", "/DNDEBUG", "/DUNICODE", "/std:c++latest", "/MT", "/O2", "/EHsc", "/nologo", "/Fe:freedom_injector.exe", "injector.cpp", "/link", "/MACHINE:x86");
-    }
 }
 
 static void build()
@@ -337,7 +174,12 @@ static void process_args(int argc, char **argv)
     parse_flags(argc, argv);
 
     if (*rebuild_flag)
-        remove_object_files();
+    {
+        FOREACH_FILE_IN_DIR(file, ".", {
+            if (ENDS_WITH(file, ".obj"))
+                RM(file);
+        });
+    }
     build();
     if (*run_flag)
         run();
@@ -364,4 +206,160 @@ int main(int argc, char **argv)
 {
     getenv("VCINSTALLDIR") ? process_args(argc, argv) : setup_vsdev_env();
     return 0;
+}
+
+static void async_obj_foreach_file_in_dir(Pid *proc, size_t *proc_count, Cstr directory)
+{
+    FOREACH_FILE_IN_DIR(file, directory,
+    {
+        if (ENDS_WITH(file, ".cpp"))
+        {
+            Cstr src = CONCAT(directory, strdup(file));
+            Cstr obj = CONCAT(NOEXT(file), ".obj");
+            if (!PATH_EXISTS(obj) ||
+                (*rebuild_flag || is_path1_modified_after_path2(src, obj)))
+            {
+                do_link = 1;
+                Cstr_Array line = *debug_flag ? cstr_array_make("cl", MSVC_DEBUG_FLAGS, src, "/c", NULL) :
+                                               cstr_array_make("cl", MSVC_RELEASE_FLAGS, src, "/c", NULL);
+                if (define_commit_hash[0] != '\0')
+                    line = cstr_array_append(line, define_commit_hash);
+                if (*debug_flag)
+                    line = cstr_array_append(line, "/DFR_DEBUG");
+                if (*console_flag)
+                    line = cstr_array_append(line, "/DFR_LOG_TO_CONSOLE");
+                Cmd cmd = {.line = line};
+                INFO("CMD: %s", cmd_show(cmd));
+                proc[(*proc_count)++] = cmd_run_async(cmd, NULL, NULL);
+            }
+        }
+    });
+}
+
+static void async_obj_foreach_file_in_dirs(Cstr first, ...)
+{
+    Pid proc[PROCESSES_CAPACITY];
+    size_t proc_count = 0;
+    async_obj_foreach_file_in_dir(proc, &proc_count, first);
+    va_list args;
+    va_start(args, first);
+    for (Cstr directory = va_arg(args, Cstr);
+         directory != NULL;
+         directory = va_arg(args, Cstr))
+    {
+        async_obj_foreach_file_in_dir(proc, &proc_count, directory);
+    }
+    va_end(args);
+    for (size_t i = 0; i < proc_count; ++i)
+        pid_wait(proc[i]);
+}
+
+static inline void set_git_commit_hash()
+{
+    HANDLE hChildStd_OUT_Rd = NULL;
+    HANDLE hChildStd_OUT_Wr = NULL;
+
+    SECURITY_ATTRIBUTES saAttr;
+    saAttr.nLength = sizeof(SECURITY_ATTRIBUTES);
+    saAttr.bInheritHandle = TRUE;
+    saAttr.lpSecurityDescriptor = NULL;
+
+    CreatePipe(&hChildStd_OUT_Rd, &hChildStd_OUT_Wr, &saAttr, 0);
+    SetHandleInformation(hChildStd_OUT_Rd, HANDLE_FLAG_INHERIT, 0);
+
+    TCHAR szCmdline[] = TEXT("git rev-parse --short HEAD");
+    PROCESS_INFORMATION piProcInfo;
+    STARTUPINFO siStartInfo;
+
+    ZeroMemory(&piProcInfo, sizeof(PROCESS_INFORMATION));
+    ZeroMemory(&siStartInfo, sizeof(STARTUPINFO));
+
+    siStartInfo.cb = sizeof(STARTUPINFO);
+    siStartInfo.hStdError = hChildStd_OUT_Wr;
+    siStartInfo.hStdOutput = hChildStd_OUT_Wr;
+    siStartInfo.hStdInput = NULL;
+    siStartInfo.dwFlags |= STARTF_USESTDHANDLES;
+
+    CreateProcess(NULL, szCmdline, NULL, NULL, TRUE, 0, NULL, NULL, &siStartInfo, &piProcInfo);
+
+    WaitForSingleObject(piProcInfo.hProcess, INFINITE);
+
+    DWORD exit_status;
+    GetExitCodeProcess(piProcInfo.hProcess, &exit_status);
+
+    CloseHandle(piProcInfo.hProcess);
+    CloseHandle(piProcInfo.hThread);
+    CloseHandle(hChildStd_OUT_Wr);
+
+    if (exit_status == 0)
+    {
+        DWORD dwRead;
+        char git_commit_hash[16] = {0};
+        ReadFile(hChildStd_OUT_Rd, git_commit_hash, 7, &dwRead, NULL);
+        if (dwRead > 0)
+            stbsp_snprintf(define_commit_hash, sizeof(define_commit_hash), "/DGIT_COMMIT_HASH=%s", git_commit_hash);
+    }
+}
+
+static inline bool find_osu_exe(LPWSTR osu_exe_path)
+{
+    PWSTR roaming_path;
+    if (SHGetKnownFolderPath(&FOLDERID_RoamingAppData, 0, NULL, &roaming_path) != S_OK)
+        return false;
+    const wchar_t *append_osu_lnk = L"\\Microsoft\\Windows\\Start Menu\\Programs\\osu!.lnk";
+    size_t new_length = wcslen(roaming_path) + wcslen(append_osu_lnk) + sizeof(WCHAR);
+    LPVOID new_ptr = CoTaskMemRealloc(roaming_path, new_length * sizeof(WCHAR));
+    if (!new_ptr)
+    {
+        CoTaskMemFree(roaming_path);
+        goto return_false;
+    }
+    roaming_path = (PWSTR)new_ptr;
+    wcscat_s(roaming_path, new_length, append_osu_lnk);
+    IShellLinkW *psl;
+    CoInitialize(NULL);
+    HRESULT hr = CoCreateInstance(&CLSID_ShellLink, NULL, CLSCTX_INPROC_SERVER, &IID_IShellLinkW, (LPVOID *)&psl);
+    if (SUCCEEDED(hr))
+    {
+        IPersistFile *ppf;
+        hr = IShellLinkW_QueryInterface(psl, &IID_IPersistFile, &ppf);
+        if (SUCCEEDED(hr))
+        {
+            hr = IPersistFile_Load(ppf, roaming_path, STGM_READ);
+            if (SUCCEEDED(hr))
+            {
+                WIN32_FIND_DATAW wfd;
+                IShellLinkW_GetPath(psl, osu_exe_path, MAX_PATH * 2, &wfd, SLGP_UNCPRIORITY | SLGP_RAWPATH);
+            }
+            else
+                goto return_false;
+        }
+        else
+            goto return_false;
+    }
+    else
+        goto return_false;
+
+    CoUninitialize();
+    CoTaskMemFree(roaming_path);
+    return true;
+
+return_false:
+    CoUninitialize();
+    CoTaskMemFree(roaming_path);
+    return false;
+}
+
+static inline bool launch_osu(LPWSTR osu_exe_path)
+{
+    STARTUPINFOW siStartInfo;
+    ZeroMemory(&siStartInfo, sizeof(siStartInfo));
+    siStartInfo.cb = sizeof(STARTUPINFOW);
+    siStartInfo.hStdError = GetStdHandle(STD_ERROR_HANDLE);
+    siStartInfo.hStdOutput = GetStdHandle(STD_OUTPUT_HANDLE);
+    siStartInfo.hStdInput = GetStdHandle(STD_INPUT_HANDLE);
+    siStartInfo.dwFlags |= STARTF_USESTDHANDLES;
+    PROCESS_INFORMATION piProcInfo;
+    ZeroMemory(&piProcInfo, sizeof(PROCESS_INFORMATION));
+    return CreateProcessW(NULL, osu_exe_path, NULL, NULL, TRUE, 0, NULL, NULL, &siStartInfo, &piProcInfo);
 }
