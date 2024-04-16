9c91a79241e91b59bed21d74a520a01a3ce6c5dd
Ciremun <Ciremun@yandex.ru>
2024-04-17 01:36:46 +0300

    bump version

--- include/freedom.h
+++ include/freedom.h
@@ -13,7 +13,7 @@
 #define GIT_COMMIT_HASH Unknown
 #endif // GIT_COMMIT_HASH
 
-#define FR_VERSION "v0.94.1 DEV [" STR(GIT_COMMIT_HASH) "]"
+#define FR_VERSION "v0.94.2 DEV [" STR(GIT_COMMIT_HASH) "]"
 
 extern HWND g_hwnd;
 extern HMODULE g_module;
